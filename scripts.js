691a27c89806e083621e4af6342cfcfa6dbed1d1
KristinaMacias <85779959+KristinaMacias@users.noreply.github.com>
2021-09-07 20:36:56 -0700

    First commit
    first commit after assigning team roles

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//Search for city (form)-- Janel
+//get request
+//method location search
+
+//render the data form the search to the table - Ritchie
+//empty array of cities searched
+//push that array to the table
+
+//add city - create an add button - Kristina
+//this is a function on the table
+
+//delete - create a delete button - Jayme
+//this is a function on the table
